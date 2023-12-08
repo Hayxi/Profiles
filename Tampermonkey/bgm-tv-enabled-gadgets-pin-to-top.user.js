b96292a907614f15e926dcb0b709215ae0b68a84
Hayxi <hayxiwong@gmail.com>
2023-12-09 01:29:30 +0800

    Create bgm-tv-enabled-gadgets-pin-to-top.user.js

---
+++ Tampermonkey/bgm-tv-enabled-gadgets-pin-to-top.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name 将已启用的组件排到最前面
+// @description 将已启用的组件排到最前面
+// @version 1.0.0
+// @author Hayxi
+// @namespace https://bgm.tv
+// @updateURL https://gist.githubusercontent.com/Hayxi/c968b08ee4d0ac003272f013826b104c/raw/bgm-tv-gadgets.user.js
+// @downloadURL https://gist.githubusercontent.com/Hayxi/c968b08ee4d0ac003272f013826b104c/raw/bgm-tv-gadgets.user.js
+// @match *://bangumi.tv/settings/gadgets
+// @match *://bgm.tv/settings/gadgets
+// @match *://chii.in/settings/gadgets
+// @run-at document-end
+// ==/UserScript==
+(() => {
+    $('#timeline ul').each((a) => {
+        var activeItem = []
+        $('#timeline ul:eq(' + a + ') .btnRedSmall').each((i) => {
+            activeItem.push($('#timeline ul:eq(' + a + ') .btnRedSmall').eq(i).parent().parent())
+        })
+        var firstItem = $('#timeline ul:eq(' + a + ') .btnGreenSmall:eq(0)').parent().parent()
+        for (var i = 0; i < activeItem.length; i++) {
+            $(firstItem).before($(activeItem[i]))
+        }
+        if(a!=0)
+        console.log("已启用" + activeItem.length + "个组件")
+    })
+})()
